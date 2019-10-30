61f1abe28db252452f878c6707043c19f1905c09
Cameron Campeau <cameron.campeau@mail.utoronto.ca>
2019-10-30 12:32:46 -0400

    Added routes to app.js, fixed github username. Cryptomillweb -> Cameron Campeau

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
 const express = require("express"),
-    app = express();
+    app = express(),
+    routes = {
+        public: require("./routes/public"),
+        api: require("./routes/api")
+    }
 
 
+app.use("/api", route.api);
+app.use("/", routes.public);
+
+
+// Start HTTP server on port 80
+app.listen(80);
