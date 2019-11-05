60bd0bed6e22f36dad0c707fa1f1459e4e349654
Jackie Huang <jackie.huang@mail.utoronto.ca>
2019-11-04 21:01:50 -0500

    Added skeleton of a profile page

--- src/public/src/App.tsx
+++ src/public/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Home, Login, Browse } from './views';
+import { Home, Login, Browse, ImportExcel, Profile } from './views';
 import { Router } from '@reach/router';
 import './App.css';
 
@@ -9,6 +9,8 @@ const App: React.FC = () => {
       <Home path="/"/>
       <Login path="login" />
       <Browse path="browse" />
+      <ImportExcel path="import" />
+      <Profile path="profile" />
     </Router>
   );
 }
--- src/public/src/views/index.js
+++ src/public/src/views/index.js
@@ -1,3 +1,5 @@
 export { default as Home } from './Home';
 export { default as Login } from './Login';
 export { default as Browse } from './Browse';
+export { default as ImportExcel } from './ImportExcel';
+export { default as Profile } from './Profile';
