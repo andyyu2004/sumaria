aa6e145f7e962df3b232aa5441718102c7f96a63
Andy Yu <andyyu2004@gmail.com>
2019-11-04 21:55:08 -0500

    Added starter for sidebar

--- src/public/src/App.tsx
+++ src/public/src/App.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
 import { Home, Login, Reset, Browse, ResetSent, AddEvent, Registration } from './views';
-import { Router } from '@reach/router';
+import { Router, navigate } from '@reach/router';
 import './App.css';
 import { Header } from './components';
 import ChatView from './views/ChatView';
+import Sidebar from './components/Sidebar';
 
 const App: React.FC = () => {
+
+  /* Pass the navigate function as a callback */
+  const sidebarEntries: [string, () => void][] = [
+    ["Chat", () => navigate('/chat')],
+    ["Browse", () => navigate('/browse')],
+    ["View2", () => {}],
+  ];
+
   return (
-    <div>
-      <Header />
-      <Router>
-        <Home path="/" />
-        <Login path="login" />
-        <Reset path="reset" />
-        <Browse path="browse" />
-        <ChatView path="chat" />
-        <ResetSent path="reset/sent" />
-        <AddEvent path="addevent" />
-        <Registration path="register" />
-      </Router>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <Header title="Sumaria" />
+      <div style={{ display: "flex", top: "70px", position: "absolute" }}>
+        <Sidebar entries={sidebarEntries} />
+        <Router style={{ flex: 7 }}>
+          <Home path="/" />
+          <Login path="login" />
+          <Reset path="reset" />
+          <Browse path="browse" />
+          <ChatView path="chat" />
+          <ResetSent path="reset/sent" />
+          <AddEvent path="addevent" />
+          <Registration path="register" />
+        </Router>
+      </div>  
     </div>
   );
 };
