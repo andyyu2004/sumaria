b7ebf291b269c3905e7e98960d331385bf7c86f8
Andy Yu <andyyu2004@gmail.com>
2019-11-04 21:28:22 -0500

    Fixed bad conflict fix

--- src/public/src/views/index.ts
+++ src/public/src/views/index.ts
@@ -4,8 +4,4 @@ export { default as Browse } from './Browse';
 export { default as AddEvent } from './AddEvent';
 export { default as Reset} from './Reset';
 export { default as ResetSent} from './ResetSent';
-<<<<<<< HEAD:src/public/src/views/index.js
 export { default as Registration} from './Registration';
-=======
-export { default as ChatView } from './ChatView';
->>>>>>> d86a86fe560250866710703b4192c3730ebe4717:src/public/src/views/index.ts
