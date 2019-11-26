26104c7ed86431115d0aa25635b05aa723638751
Cameron Campeau <cameron.campeau@mail.utoronto.ca>
2019-11-26 17:39:54 -0500

    Public routes

--- src/routes/public.js
+++ src/routes/public.js
@@ -1,3 +1,11 @@
-const router = require("express").Router();
+const express = require("express"), 
+    router = express.Router(),
+    path = require("path");
+
+router.use("/", (req,res) => {
+    res.sendFile(path.resolve(__dirname + "../public/public/index.html"));
+});
+
+router.use("/build", express.static(path.resolve(__dirname + "/../public/build")));
 
 module.exports = router;
