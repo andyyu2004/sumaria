da841728d02fb26b138c2f8ce550b26b94c93c6f
mouisaac <isaac.mou@mail.utoronto.ca>
2019-12-01 15:46:26 -0500

    implemented waitlist logics for viewEvent;

--- src/public/src/views/ViewEvent.jsx
+++ src/public/src/views/ViewEvent.jsx
@@ -41,6 +41,29 @@ const ViewEvent = props => {
 
   const user = useUser();
 
+  /*
+  const [registeredNum, setRegisteredNum] = useState(registeredParticipants.length);
+  const [waitlistNum, setWaitlistNum] = useState(0);
+  const [waitlisted, setWaitlisted] = useState(false);
+  */
+  
+  let registeredNum = registeredParticipants.length;
+  let waitlistNum = 0;
+  let waitlisted = false;
+  let overflow;
+  let waitlistStart = false;
+  
+  if (registeredNum >= numVolunteers){
+    waitlisted = true;
+    if (registeredNum > numVolunteers){
+      waitlistStart = true;
+    }
+    registeredNum = numVolunteers;
+    waitlistNum = registeredNum - numVolunteers;
+  }
+
+
+
   const isCreator = creatorid === user._id;
 
   const uploadFile = async e => {
@@ -77,8 +100,9 @@ const ViewEvent = props => {
   const checkIsUserRegistered = useCallback(participants => {
     //console.log(participants);
     setRegisteredParticipants(participants);
-    if (participants.find(x => x._id === user._id) && !isRegistered) {
-      setIsRegistered(true);
+    let userRegisterIndex = participants.findIndex(x => x._id === user._id);
+    if (userRegisterIndex >= 0 && !isRegistered) {
+      setIsRegistered(userRegisterIndex + 1);
     }
   }, [isRegistered, user._id]);
 
@@ -130,11 +154,13 @@ const ViewEvent = props => {
       {/* {files.map(f => <div key={f._id} onClick={() => downloadFile(f._id)}>{f.file.name}</div>)} */}
       {files.map(f => <div key={f._Id}><a href={`/api/event/${eventId}/file/${f._id}`}>{f.file.name}</a><br /></div>)}
       {isCreator && <input type="file" onChange={uploadFile} multiple />}
-      { isRegistered ? <Button style={{float: 'right'}} onClick={() => cancelEventRegistration()}>Cancel</Button> : <Button style={{float: 'right'}} onClick={() => registerEvent()}>Register</Button>}
+      { isRegistered ? <Button style={{float: 'right'}} onClick={() => cancelEventRegistration()}>{(isRegistered - numVolunteers) ? 'Cencel Waitlist' : 'Cancel'}</Button> : <Button style={{float: 'right'}} onClick={() => registerEvent()}>{waitlisted ? 'Waitlist' : 'Register'}</Button>}
       {/* Show button to add event file if the user is the creator of the event */}
       <br/><br/><br/>
-      {isCreator && <h3>{registeredParticipants.length}/{numVolunteers} Participants</h3>}
-      {isCreator ? (registeredParticipants.map(p => <div key={p._id}><span>{p.firstname + ' ' + p.lastname}</span><i style={{marginLeft: '5px', cursor: 'pointer'}} className="fas fa-user" onClick={()=>publicProfile(p.username)}></i><br /></div>)) : null}
+      {<h3>{waitlistStart ? registeredParticipants.length - numVolunteers : registeredParticipants.length}/{numVolunteers} Participants {waitlisted ? '(' + (registeredParticipants.length - numVolunteers) + ' Waitlisted)' : null}</h3>}
+      <ol type="1">
+      {isCreator ? (registeredParticipants.map(p => <li key={p._id}>{p.firstname + ' ' + p.lastname}<i style={{marginLeft: '5px', cursor: 'pointer'}} className="fas fa-user" onClick={()=>publicProfile(p.username)}></i><br /></li>)) : null}
+      </ol>
     </div>
   </div>
   );
