ec44ff23de3c9a62c8bc15122970df2264cf89c6
Cameron Campeau <cameron.campeau@mail.utoronto.ca>
2019-11-30 22:36:51 -0500

    Home page title fix

--- src/public/src/views/Home.jsx
+++ src/public/src/views/Home.jsx
@@ -5,7 +5,7 @@ const Home = props => {
 
   return (
     <div>
-      <h2 className="m-3">Home</h2>
+      <h2 className="m-3 text-center">Home</h2>
       <div class="row mx-3">
         <Link to="browse" className="col-6">
           <div className="py-5 mx-1 mb-3 shadow-sm text-center justify-content-center align-items-center d-flex bg-light">
