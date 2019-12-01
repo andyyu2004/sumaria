d59ac8fc1b3380caad992175611e1b9c3be95be7
Cameron Campeau <cameron.campeau@mail.utoronto.ca>
2019-11-30 22:25:29 -0500

    Remove hotfix

--- src/routes/public.js
+++ src/routes/public.js
@@ -2,12 +2,10 @@ const express = require("express"),
     router = express.Router(),
     path = require("path");
 
-router.use("/build", express.static(path.resolve(__dirname + "/../public/build")));
+router.use(express.static(path.resolve(__dirname + "/../public/build")));
 
 router.use("/", (req,res) => {
-    console.log("Serving root");
     res.sendFile(path.resolve(__dirname + "/../public/build/index.html"));
 });
-
 
 module.exports = router;
