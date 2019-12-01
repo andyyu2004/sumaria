d96ad976861ec54401f26e4ed61fa6d855a4fa32
Cameron Campeau <cameron.campeau@mail.utoronto.ca>
2019-11-30 22:04:42 -0500

    Home page design update

--- src/public/src/views/Home.jsx
+++ src/public/src/views/Home.jsx
@@ -5,10 +5,24 @@ const Home = props => {
 
   return (
     <div>
-      <h2>Home</h2>
-      <Link to="browse">Browse for events</Link><br />
-      <Link to='import'>Import Excel file</Link><br />
-      <Link to='addevent'>Add event</Link><br />
+      <h2 className="m-3">Home</h2>
+      <div class="row mx-3">
+        <Link to="browse" className="col-6">
+          <div className="py-5 mx-1 mb-3 shadow-sm text-center justify-content-center align-items-center d-flex bg-light">
+            <h4 className="py-5"><i className="fa fa-calendar-alt mr-2"></i>Browse for events</h4>
+          </div>
+        </Link>
+        <Link to="import" className="col-6">
+          <div className="py-5 mx-1 mb-3 shadow-sm text-center justify-content-center align-items-center d-flex bg-light">
+            <h4 className="py-5"><i className="fa fa-upload mr-2"></i>Import Excel file</h4>
+          </div>
+        </Link>
+        <Link to="addevent" className="col-6">
+          <div className="py-5 mx-1 mb-3 shadow-sm text-center justify-content-center align-items-center d-flex bg-light">
+            <h4 className="py-5"><i className="fa fa-plus mr-2"></i>Add event</h4>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 }
